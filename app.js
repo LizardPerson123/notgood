dd3a0a7db24e71b95de661321871f4fdc65be0bb
LizardPerson123 <dylanerck@1203landmark.com>
2025-07-15 16:53:38 -0500

    Add files via upload

--- app.js
+++ app.js
@@ -4,5 +4,7 @@ const express = require("express")
 const app = express()
 
 app.get("/", function(req, res) {
-  fs.readFile("/index.html")
+  fs.readFile("/index.html", function(er, data) {
+    res.send(data)
+  })
 })
